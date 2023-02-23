bd086eb8b37fbeb1f6edc8ec4d0d4215d5a68c66
theujjwal <66766108+theujjwal@users.noreply.github.com>
2023-02-23 18:24:24 +0530

    addtocart removetocart

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,15 +2,23 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+  const DUMMY_PRODUCT = [
+    {id:'p1',price: 6, title: 'First Book', description: 'yoyoyoy'},
+    {id:'p2',price: 7, title: 'second Book', description: 'momomom'},
+  ]
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCT.map((product) => 
+        (<ProductItem
+         key = {product.id}
+         id = {product.id}
+          title = {product.title}
+          price={product.price}
+          description={product.description}
+        />))}
+
       </ul>
     </section>
   );
---
+++ src/store/ui-slice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const uiSlice = createSlice({
+    name:'ui',
+    initialState:{cartVisible:false},
+    reducers:{
+        toggle(state){
+            state.cartVisible=!state.cartVisible
+        }
+    }
+})
+export const uiActions = uiSlice.actions
+export default uiSlice.reducer
